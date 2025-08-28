9963bb81a7b3d2938a7d935f31f8960b6ae1b6b9
Sebastian Herrlinger <hasta84@gmail.com>
2025-08-29 00:22:42 +0200

    v0.1.12

--- packages/templates/core/src/index.ts
+++ packages/templates/core/src/index.ts
@@ -1,51 +1,20 @@
 import {
-  ASCIIFontRenderable,
-  BoxRenderable,
-  type CliRenderer,
+  ASCIIFont,
+  Box,
   createCliRenderer,
+  Text,
   TextAttributes,
-  TextRenderable,
 } from "@opentui/core";
 
-class App {
-  private outerBox: BoxRenderable;
-  private innerBox: BoxRenderable;
-  private asciiFont: ASCIIFontRenderable;
-  private text: TextRenderable;
-  private renderer: CliRenderer;
-
-  constructor(renderer: CliRenderer) {
-    this.renderer = renderer;
-
-    this.outerBox = new BoxRenderable("outer-container", {
-      alignItems: "center",
-      justifyContent: "center",
-      flexGrow: 1,
-    });
-
-    this.innerBox = new BoxRenderable("inner-container", {
-      justifyContent: "center",
-      alignItems: "flex-end",
-    });
-
-    this.asciiFont = new ASCIIFontRenderable("ascii-font", {
-      font: "tiny",
-      text: "OpenTUI",
-    });
-
-    this.text = new TextRenderable("text", {
-      content: "What will you build?",
-      attributes: TextAttributes.DIM,
-    });
-
-    this.innerBox.add(this.asciiFont);
-    this.innerBox.add(this.text);
-    this.outerBox.add(this.innerBox);
-    this.renderer.root.add(this.outerBox);
-
-    this.renderer.start();
-  }
-}
-
 const renderer = await createCliRenderer({ exitOnCtrlC: true });
-new App(renderer);
+
+renderer.root.add(
+  Box(
+    { alignItems: "center", justifyContent: "center", flexGrow: 1 },
+    Box(
+      { justifyContent: "center", alignItems: "flex-end" },
+      ASCIIFont({ font: "tiny", text: "OpenTUI" }),
+      Text({ content: "What will you build?", attributes: TextAttributes.DIM })
+    )
+  )
+);
